2d07420c23968a6a042ede0d20bb13d878da3b47
JieChongHai <3357991893@qq.com>
2024-01-13 13:48:45 +0800

    chore: update ngrx

--- src/app/store/store-devtools/index.ts
+++ src/app/store/store-devtools/index.ts
@@ -1,8 +1,9 @@
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools'
 
 export const extModules = [
-  StoreDevtoolsModule.instrument({ 
+  StoreDevtoolsModule.instrument({
     maxAge: 25, // Retains last 25 states
     autoPause: true, // Pauses recording actions and state changes when the extension window is not open
-   }),
-];
+    connectInZone: true,
+  }),
+]
